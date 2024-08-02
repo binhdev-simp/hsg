509dab1207e9b2f9e0f722c23d53f61721001ba4
ianTug <dinhducbinh2k7@gmail.com>
2024-08-02 10:03:23 +0700

    update

--- maxsum.py
+++ maxsum.py
@@ -1,17 +1,11 @@
 import math
-n = int(input("nhap so n: "))
-c=0
-gcdSum=0
-tpm=0
-def gcd(x,y):
-    return math.gcd(x,y)
-#main
-if 3<=n<=10**14:
-    for m in range(1,n):
-        gcdSum = gcd(m,n)+m
-        if gcdSum >= tpm:
-            if gcdSum>tpm:
-                tpm = gcdSum
-            if m>c: c=m
-        print(gcdSum, tpm,m,c, gcd(m,n))
-# print(tpm)
+import time
+n = int(input(''))
+st = time.time()
+begin = n-round(math.sqrt(n))
+end = n-1
+l = {(math.gcd(n,m)+m) : m for m in range(begin,end)}
+kq = str(l[max(l)])
+ed = time.time()
+print(kq,':',max(l))
+print('Time run:',ed-st)
